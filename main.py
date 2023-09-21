3a58bdbbd4091c9693fbca4616979bbb602491e5
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2023-09-21 00:11:51 +0000

    25501. 재귀의 귀재

---
+++ main.py
@@ -0,0 +1,20 @@
+def recursion(s, l, r):
+  global numOfRecursionCall
+  if l >= r:
+    return 1
+  elif s[l] != s[r]:
+    return 0
+  else:
+    numOfRecursionCall += 1
+    return recursion(s, l + 1, r - 1)
+
+
+def isPalindrome(s):
+  return recursion(s, 0, len(s) - 1)
+
+
+t = int(input())
+for _ in range(t):
+  s = input()
+  numOfRecursionCall = 1
+  print(isPalindrome(s), numOfRecursionCall)
