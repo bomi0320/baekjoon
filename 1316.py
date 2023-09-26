31791fd6761b47374874a8162e93307abedab244
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2023-09-26 14:13:48 +0000

    1316. 그룹 단어 체커

--- 1316.py
+++ 1316.py
@@ -1,19 +1,19 @@
 result = 0
 
+
 def f(s):
   global result
   l = len(string)
   alpha = [s[0]]
-  for i in range(1,l):
-    if s[i] == s[i-1]:
+  for i in range(1, l):
+    if s[i] == s[i - 1]:
       continue
     elif s[i] in alpha:
       return
     else:
       alpha.append(s[i])
   result += 1
-      
-    
+
 
 t = int(input())
 for _ in range(t):
