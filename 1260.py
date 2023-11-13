119bf749cf3991b5ab82185238715c2e641670ac
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2023-11-13 23:49:35 +0900

    DFS와 BFS

---
+++ 1260.py
@@ -0,0 +1,42 @@
+# DFS와 BFS
+
+from collections import deque
+
+
+def dfs(graph, v, visitedj):
+    visited[v] = True
+    print(v, end=" ")
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+def bfs(graph, v, visited):
+    queue = deque([v])
+    visited[v] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
+
+
+visited = [False] * (n+1)
+dfs(graph, v, visited)
+print()
+
+visited = [False] * (n+1)
+bfs(graph, v, visited)
