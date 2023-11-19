31c344d2ca121f715105588816f94d7dbd689d86
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2023-11-19 21:55:11 +0900

    카드2

--- 2164.py
+++ 2164.py
@@ -1,16 +1,13 @@
 # 카드2
+
 from collections import deque
 
 n = int(input())
 
 queue = deque([i for i in range(1, n+1)])
-#print(queue)
 
 while len(queue) != 1:
     queue.popleft()
-    if len(queue) == 1:
-        break
-    queue.append(queue[0])
-    queue.popleft()
+    queue.append(queue.popleft())
 
 print(queue[0])
