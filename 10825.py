91d262d5911c8a19ffa24502bbd13391646f5e52
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-03-05 23:01:41 +0900

    국영수

---
+++ 10825.py
@@ -0,0 +1,14 @@
+# 국영수
+import sys
+
+n = int(input())
+students = []
+for _ in range(n):
+    name, korean, english, math = sys.stdin.readline().split()
+    students.append([name, int(korean), int(english), int(math)])
+
+students.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for student in students:
+    print(student[0])
+
