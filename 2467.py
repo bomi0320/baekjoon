e462acd7441181f02e5b31b46291d7acb21932b8
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-03-17 08:56:13 +0900

    용액

---
+++ 2467.py
@@ -0,0 +1,23 @@
+# 용액
+import math
+
+n = int(input())
+data = list(map(int, input().split()))
+
+min_val = math.inf
+min_data = [0, 0]
+l, r = 0, n-1
+while l < r:
+    tmp = data[l] + data[r]
+
+    if abs(tmp) < abs(min_val):
+        min_val = tmp
+        min_data[0], min_data[1] = data[l], data[r]
+
+    if tmp < 0:
+        l += 1
+    else:
+        r -= 1
+
+print(min_data[0], min_data[1])
+
