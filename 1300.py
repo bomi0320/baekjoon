45564de32e35870a98ec3dd2ac399b638c9b4f8d
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-08 14:50:17 +0900

    K번째 수

--- 1300.py
+++ 1300.py
@@ -1,24 +1,20 @@
 # K번째 수
 
-N = int(input())
+n = int(input())
 k = int(input())
 
-start = 1
-end = k
-result = -1
+start, end = 1, k
+result = 0
 
 while start <= end:
-    mid = (start + end) // 2
-    tmp = 0
-    for i in range(1, N+1):
-        if mid // i > N:
-            tmp += N
-        else:
-            tmp += mid // i
-    if tmp < k:
-        start = mid + 1
+    middle = (start + end) // 2
+    temp = 0
+    for i in range(1, n + 1):
+        temp += min(n, middle//i)
+    if temp < k:
+        start = middle + 1
     else:
-        end = mid - 1
-        result = mid
+        result = middle
+        end = middle - 1
 
 print(result)
