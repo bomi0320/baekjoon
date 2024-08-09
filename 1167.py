6b08404c12e50b2174b7319585a85538ea53ae6c
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-09 16:05:45 +0900

    트리의 지름

--- 1167.py
+++ 1167.py
@@ -1,50 +1,49 @@
 # 트리의 지름
 
 import sys
-from collections import deque
 
 n = int(input())
-A = [[] for _ in range(n + 1)]
-
-
-def bfs():
-    farthest = (0, 0)  # node, distance
-    while len(queue) != 0:
-        this_node, this_weight = queue.popleft()
-        visited[this_node] = True
-        if this_weight > farthest[1]:
-            farthest = (this_node, this_weight)
-        for component_node, component_weight in A[this_node]:
-            if not visited[component_node]:
-                queue.append((component_node, component_weight + this_weight))
-    return farthest
-
+A = [[] for _ in range(n+1)]
 
 for _ in range(n):
     line = list(map(int, sys.stdin.readline().split()))
     v = line[0]
     for i in range(1, len(line), 2):
-        u = line[i]
-        if u == -1:
+        if line[i] == -1:
             break
         else:
-            weight = line[i+1]
-            A[v].append((u, weight))
+            u, w = line[i], line[i+1]
+            A[v].append((u, w))
 
-queue = deque()
 
-# bfs(1): 1번 노드에서 가장 먼 노드 찾기
+def dfs():
+    farthest_distance, farthest_node = 0, 0
+    while len(stack):
+        this_node, this_weight = stack.pop()
+        visited[this_node] = True
+        if this_weight > farthest_distance:
+            farthest_distance = this_weight
+            farthest_node = this_node
+        for next_node, next_weight in A[this_node]:
+            if not visited[next_node]:
+                stack.append((next_node, next_weight + this_weight))
+    return farthest_distance, farthest_node
+
+
+stack = []
+
+# dfs(1)
 visited = [False] * (n + 1)
+for i in A[1]:
+    stack.append(i)
 visited[1] = True
-for component in A[1]:
-    queue.append(component)
-farthest_node = bfs()[0]
+f_distance, f_node = dfs()
 
-# bfs(farthest_node)
+# dfs(f_node)
 visited = [False] * (n + 1)
-visited[farthest_node] = True
-for component in A[farthest_node]:
-    queue.append(component)
-farthest_distance = bfs()[1]
+for i in A[f_node]:
+    stack.append(i)
+visited[f_node] = True
+f_distance, f_node = dfs()
 
-print(farthest_distance)
+print(f_distance)
