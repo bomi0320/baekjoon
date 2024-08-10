a849d55b1bc3c5a55ee8d5b497e64a5a5bf11e40
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-10 17:29:57 +0900

    기타 레슨

--- 2343.py
+++ 2343.py
@@ -3,25 +3,26 @@
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
-left, right = max(A), sum(A)
 
-
-def NumOfBlueRay(length):
-    result = 1
+def NumOfBlueRay(size):
+    blue_ray = 1
     temp = 0
     for i in A:
         temp += i
-        if temp > length:
+        if temp > size:
             temp = i
-            result += 1
-    return result
+            blue_ray += 1
+    return blue_ray
 
 
+left, right = max(A), sum(A)
 while left <= right:
-    middle = (left + right) // 2
-    if NumOfBlueRay(middle) <= m:
-        right = middle - 1
+    median = (left + right) // 2
+    num_of_blue_ray = NumOfBlueRay(median)
+    if num_of_blue_ray <= m:
+        right = median - 1
     else:
-        left = middle + 1
+        left = median + 1
 
 print(left)
+
