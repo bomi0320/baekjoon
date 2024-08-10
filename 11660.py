b423f3d0c8fb0b62ba935062cf03647895e0497c
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-10 16:02:39 +0900

    구간 합 구하기 5

--- 11660.py
+++ 11660.py
@@ -1,24 +1,21 @@
 # 구간 합 구하기 5
+
 import sys
-input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-# 표 채우기
-graph = []
+A = [[0 for _ in range(n+1)]]
 for _ in range(n):
-    tmp = list(map(int, input().split()))
-    graph.append(tmp)
+    line = list(map(int, sys.stdin.readline().split()))
+    line.insert(0, 0)
+    A.append(line)
 
-# 합 배열 만들기
+# 구간 합 구하기
 S = [[0 for _ in range(n+1)] for _ in range(n+1)]
-
 for i in range(1, n+1):
     for j in range(1, n+1):
-        S[i][j] = S[i][j-1] + S[i-1][j] + graph[i-1][j-1] - S[i-1][j-1]
+        S[i][j] = S[i][j-1] + S[i-1][j] - S[i-1][j-1] + A[i][j]
 
-# 구간의 합 구하기
 for _ in range(m):
-    x1, y1, x2, y2 = map(int, input().split())
-    result = S[x2][y2] - S[x2][y1-1] - S[x1-1][y2] + S[x1-1][y1-1]
-    print(result)
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    print(S[x2][y2] - S[x2][y1-1] - S[x1-1][y2] + S[x1-1][y1-1])
