4ca66a28db320ade88939bfce16c86bc7851d197
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-13 15:35:05 +0900

    주몽

--- 1940.py
+++ 1940.py
@@ -1,3 +1,23 @@
 # 주몽
 
+n = int(input())
+m = int(input())
+A = list(map(int, input().split()))
 
+A.sort()
+
+count = 0
+left, right = 0, n-1
+
+while left < right:
+    temp = A[left] + A[right]
+    if temp == m:
+        count += 1
+        left += 1
+        right -= 1
+    elif temp > m:
+        right -= 1
+    else:
+        left += 1
+
+print(count)
