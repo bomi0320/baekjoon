6e52a91fbc8bb9331947ad761fa0c85c8e1bed15
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-13 15:25:09 +0900

    나머지 합

--- 10986.py
+++ 10986.py
@@ -1,32 +1,19 @@
 # 나머지 합
 
-from collections import Counter
-import math
-
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
-# 구간 합
+# 구간 합 구하기
 S = [A[0]]
 for i in range(1, n):
     S.append(S[i-1] + A[i])
 
-# 구간 합의 나머지
-remainder_of_S = []
-result = 0
-for i in range(n):
-    remainder = S[i] % m
-    remainder_of_S.append(remainder)
-    if remainder == 0:  # 0부터 i까지의 합의 나머지가 0인 것
-        result += 1
+remainder = [0 for _ in range(m)]
+for i in S:
+    remainder[i % m] += 1
 
-counter = Counter(remainder_of_S)
-
-for key, value in counter.items():
-    nCr = math.comb(value, 2)
-    result += nCr
+result = remainder[0]
+for i in remainder:
+    result += i * (i-1) // 2
 
 print(result)
-
-
-
--- 1940.py
+++ 1940.py
@@ -1,24 +1,3 @@
 # 주몽
-n = int(input())
-m = int(input())
-mater = list(map(int, input().split()))
 
-mater.sort()
 
-i = 0
-j = n-1
-sum = mater[i] + mater[j]
-cnt = 0
-
-while i < j:
-    if sum > m:
-        j -= 1
-    elif sum < m:
-        i += 1
-    else:  # sum == n
-        i += 1
-        j -= 1
-        cnt += 1
-    sum = mater[i] + mater[j]
-
-print(cnt)
