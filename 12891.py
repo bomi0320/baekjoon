be86a82d1f873926177d47a490f8b656fc447945
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-16 11:52:38 +0900

    DNA 비밀번호

--- 12891.py
+++ 12891.py
@@ -1,3 +1,22 @@
 # DNA 비밀번호
 
+from collections import Counter
 
+s, p = map(int, input().split())
+DNA = input()
+a, c, g, t = map(int, input().split())
+
+result = 0
+
+# 첫번째 부분문자열
+counter = Counter(DNA[0:p])
+if counter['A'] >= a and counter['C'] >= c and counter['G'] >= g and counter['T'] >= t:
+    result += 1
+
+for i in range(1, s-p+1):
+    counter[DNA[i-1]] -= 1
+    counter[DNA[i+p-1]] += 1
+    if counter['A'] >= a and counter['C'] >= c and counter['G'] >= g and counter['T'] >= t:
+        result += 1
+
+print(result)
