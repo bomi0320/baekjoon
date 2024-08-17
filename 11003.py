bc38798cb2c0b6fded9e7c13602919854e00b4a2
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-17 16:42:11 +0900

    최솟값 찾기

--- 11003.py
+++ 11003.py
@@ -1,15 +1,19 @@
 # 최솟값 찾기
+
 from collections import deque
 
 N, L = map(int, input().split())
-nums = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
-queue = deque([])
+deque = deque()
+
 for i in range(N):
-    while queue and queue[-1][1] >= nums[i]:
-        queue.pop()
-    queue.append((i+1, nums[i]))
-    if queue[0][0] <= i+1-L:
-        queue.popleft()
-
-    print(queue[0][1], end=" ")
+    for j in range(len(deque)-1, -1, -1):
+        if deque[j][1] < A[i]:
+            break
+        else:
+            deque.pop()
+    deque.append((i, A[i]))
+    if deque[0][0] <= i-L:
+        deque.popleft()
+    print(deque[0][1], end=' ')
