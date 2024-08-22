a12506279a1b52e7b55b4570d2c2129a69bc2fb7
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-22 16:05:34 +0900

    최솟값 찾기

--- 11003.py
+++ 11003.py
@@ -5,15 +5,15 @@ from collections import deque
 N, L = map(int, input().split())
 A = list(map(int, input().split()))
 
-deque = deque()
-
-for i in range(N):
-    for j in range(len(deque)-1, -1, -1):
-        if deque[j][1] < A[i]:
-            break
-        else:
-            deque.pop()
-    deque.append((i, A[i]))
-    if deque[0][0] <= i-L:
-        deque.popleft()
-    print(deque[0][1], end=' ')
+que = deque([1])  # index 저장
+print(A[0], end=" ")
+for i in range(2, N+1):
+    now = A[i-1]
+    if que and A[que[-1]-1] > now:
+        que.pop()
+        while que and A[que[-1]-1] > now:
+            que.pop()
+    que.append(i)
+    if que[0] < i - L + 1:
+        que.popleft()
+    print(A[que[0]-1], end=' ')
