112803993aa11a9ce13268b93e2c762e3b0668bf
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-22 15:40:54 +0900

    절댓값 힙

--- 11286.py
+++ 11286.py
@@ -1,22 +1,18 @@
 # 절댓값 힙
-import sys
+
 from queue import PriorityQueue
+import sys
 
 N = int(input())
 
-queue = PriorityQueue()
+que = PriorityQueue()
 
-for i in range(N):
-    num = int(sys.stdin.readline())
-    if num == 0:  # 절댓값이 가장 작은 값 출력
-        if queue.empty():  # 큐가 비어있을 경우
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x != 0:  # 추가
+        que.put((abs(x), x))
+    else:  # 삭제
+        if que.empty():
             print(0)
         else:
-            print(queue.get()[1])
-    else:  # 배열에 추가
-        if num < 0:  # 음수일 경우
-            # ex) -1 => 우선순위 0.5
-            queue.put((0-num-0.5, num))  # (우선순위, 값)
-        else:
-            # ex) 1 => 우선순위 1
-            queue.put((num, num))
+            print(que.get()[1])
