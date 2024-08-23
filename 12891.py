ad65e702b06206eaa80605703903037e848dfcdb
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-08-23 14:43:40 +0900

    DNA 비밀번호

--- 12891.py
+++ 12891.py
@@ -1,68 +1,67 @@
 # DNA 비밀번호
 
 S, P = map(int, input().split())
-DNA = input()
+DNA = list(input().strip())
 ACGT = list(map(int, input().split()))
 
-check_num = 0
-for i in range(4):
-    if ACGT[i] == 0:
-        check_num += 1
+this_acgt = [0, 0, 0, 0]  # A, C, G, T
+clear = 0
+count = 0
+
+for acgt in ACGT:
+    if acgt == 0:
+        clear += 1
 
 
-def increase(c):
-    global check_num
+def add(c):
+    global clear
     if c == 'A':
-        counter['A'] += 1
-        if counter['A'] == ACGT[0]:
-            check_num += 1
+        this_acgt[0] += 1
+        if this_acgt[0] == ACGT[0]:
+            clear += 1
     elif c == 'C':
-        counter['C'] += 1
-        if counter['C'] == ACGT[1]:
-            check_num += 1
+        this_acgt[1] += 1
+        if this_acgt[1] == ACGT[1]:
+            clear += 1
     elif c == 'G':
-        counter['G'] += 1
-        if counter['G'] == ACGT[2]:
-            check_num += 1
-    else:  # 'T'
-        counter['T'] += 1
-        if counter['T'] == ACGT[3]:
-            check_num += 1
+        this_acgt[2] += 1
+        if this_acgt[2] == ACGT[2]:
+            clear += 1
+    else:  # 'T
+        this_acgt[3] += 1
+        if this_acgt[3] == ACGT[3]:
+            clear += 1
 
 
-def decrease(c):
-    global check_num
+def subtract(c):
+    global clear
     if c == 'A':
-        if counter['A'] == ACGT[0]:
-            check_num -= 1
-        counter['A'] -= 1
+        if this_acgt[0] == ACGT[0]:
+            clear -= 1
+        this_acgt[0] -= 1
     elif c == 'C':
-        if counter['C'] == ACGT[1]:
-            check_num -= 1
-        counter['C'] -= 1
+        if this_acgt[1] == ACGT[1]:
+            clear -= 1
+        this_acgt[1] -= 1
     elif c == 'G':
-        if counter['G'] == ACGT[2]:
-            check_num -= 1
-        counter['G'] -= 1
-    else:  # 'T'
-        if counter['T'] == ACGT[3]:
-            check_num -= 1
-        counter['T'] -= 1
+        if this_acgt[2] == ACGT[2]:
+            clear -= 1
+        this_acgt[2] -= 1
+    else:  # 'T
+        if this_acgt[3] == ACGT[3]:
+            clear -= 1
+        this_acgt[3] -= 1
 
 
-# 초기 설정
-counter = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
 for i in range(P):
-    increase(DNA[i])
+    add(DNA[i])
+if clear == 4:
+    count += 1
 
-result = 0
-if check_num == 4:
-    result += 1
+for i in range(P, S):
+    subtract(DNA[i-P])
+    add(DNA[i])
+    if clear == 4:
+        count += 1
 
-for i in range(S-P):
-    decrease(DNA[i])
-    increase(DNA[i+P])
-    if check_num == 4:
-        result += 1
-
-print(result)
+print(count)
