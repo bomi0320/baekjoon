e28165a31ee6d4ed6ad021d5dd4ac9175b108785
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-10-09 15:43:02 +0900

    DNA 비밀번호

--- 12891.py
+++ 12891.py
@@ -1,67 +1,74 @@
 # DNA 비밀번호
 
+# input
 S, P = map(int, input().split())
-DNA = list(input().strip())
+DNA = input()
 ACGT = list(map(int, input().split()))
 
-this_acgt = [0, 0, 0, 0]  # A, C, G, T
-clear = 0
-count = 0
 
-for acgt in ACGT:
-    if acgt == 0:
-        clear += 1
-
-
+# 함수
 def add(c):
-    global clear
+    global num_of_clear
     if c == 'A':
-        this_acgt[0] += 1
-        if this_acgt[0] == ACGT[0]:
-            clear += 1
+        num_of_acgt[0] += 1
+        if num_of_acgt[0] == ACGT[0]:
+            num_of_clear += 1
     elif c == 'C':
-        this_acgt[1] += 1
-        if this_acgt[1] == ACGT[1]:
-            clear += 1
+        num_of_acgt[1] += 1
+        if num_of_acgt[1] == ACGT[1]:
+            num_of_clear += 1
     elif c == 'G':
-        this_acgt[2] += 1
-        if this_acgt[2] == ACGT[2]:
-            clear += 1
-    else:  # 'T
-        this_acgt[3] += 1
-        if this_acgt[3] == ACGT[3]:
-            clear += 1
+        num_of_acgt[2] += 1
+        if num_of_acgt[2] == ACGT[2]:
+            num_of_clear += 1
+    else:  # 'T'
+        num_of_acgt[3] += 1
+        if num_of_acgt[3] == ACGT[3]:
+            num_of_clear += 1
 
 
 def subtract(c):
-    global clear
+    global num_of_clear
     if c == 'A':
-        if this_acgt[0] == ACGT[0]:
-            clear -= 1
-        this_acgt[0] -= 1
+        if num_of_acgt[0] == ACGT[0]:
+            num_of_clear -= 1
+        num_of_acgt[0] -= 1
     elif c == 'C':
-        if this_acgt[1] == ACGT[1]:
-            clear -= 1
-        this_acgt[1] -= 1
+        if num_of_acgt[1] == ACGT[1]:
+            num_of_clear -= 1
+        num_of_acgt[1] -= 1
     elif c == 'G':
-        if this_acgt[2] == ACGT[2]:
-            clear -= 1
-        this_acgt[2] -= 1
-    else:  # 'T
-        if this_acgt[3] == ACGT[3]:
-            clear -= 1
-        this_acgt[3] -= 1
+        if num_of_acgt[2] == ACGT[2]:
+            num_of_clear -= 1
+        num_of_acgt[2] -= 1
+    else:
+        if num_of_acgt[3] == ACGT[3]:
+            num_of_clear -= 1
+        num_of_acgt[3] -= 1
 
 
+# 초기 설정
+num_of_acgt = [0, 0, 0, 0]
+num_of_clear = 0
+result = 0
+
+for acgt in ACGT:
+    if acgt == 0:
+        num_of_clear += 1
+
+
+# 첫번째 DNA 부분문자열
 for i in range(P):
     add(DNA[i])
-if clear == 4:
-    count += 1
 
-for i in range(P, S):
-    subtract(DNA[i-P])
-    add(DNA[i])
-    if clear == 4:
-        count += 1
+if num_of_clear == 4:
+    result += 1
 
-print(count)
+# DNA 부분문자열
+for i in range(1, S-P+1):
+    subtract(DNA[i-1])
+    add(DNA[i+P-1])
+    if num_of_clear == 4:
+        result += 1
+
+print(result)
